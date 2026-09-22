e5a8a1a9574895f13f91195f9415a330d8415458
antoinelrnld <38052726+antoinelrnld@users.noreply.github.com>
2026-09-22 12:11:45 +0200

    exercise 7.3

---
+++ Exercises/07/03/stock.py
@@ -0,0 +1,15 @@
+from structure import Structure, validate_attributes
+from validate import String, PositiveInteger, PositiveFloat
+
+
+class Stock(Structure):
+    name = String()
+    shares = PositiveInteger()
+    price = PositiveFloat()
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
---
+++ Exercises/07/03/structure.py
@@ -0,0 +1,50 @@
+# structure.py
+from validate import Validator
+
+class Structure:
+    _fields = ()
+    _types = ()
+
+    def __setattr__(self, name, value):
+        if name.startswith('_') or name in self._fields:
+            super().__setattr__(name, value)
+        else:
+            raise AttributeError('No attribute %s' % name)
+
+    def __repr__(self):
+        return '%s(%s)' % (type(self).__name__,
+                           ', '.join(repr(getattr(self, name)) for name in self._fields))
+
+    @classmethod
+    def create_init(cls):
+        '''
+        Create an __init__ method from _fields
+        '''
+        args = ','.join(cls._fields)
+        code = f'def __init__(self, {args}):\n'
+        for name in cls._fields:
+            code += f'    self.{name} = {name}\n'
+        locs = { }
+        exec(code, locs)
+        cls.__init__ = locs['__init__']
+    
+    @classmethod
+    def __init_subclass__(cls):
+        validate_attributes(cls)
+        
+    @classmethod
+    def from_row(cls, row):
+        rowdata = [func(val) for func, val in zip(cls._types, row)]
+        return cls(*rowdata)
+
+
+def validate_attributes(cls):
+    validators = []
+    for _, val in vars(cls).items():
+        if isinstance(val, Validator):
+            validators.append(val)
+    cls._fields = [val.name for val in validators]
+    cls._types = [val.expected_type for val in validators if val.expected_type]
+    if isinstance(cls, Structure):
+        cls.create_init()
+    return cls
